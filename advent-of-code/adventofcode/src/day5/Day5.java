91c525b8f75d711bbad72a2f5dd1a86931350847
Atom <atomcraft.4u@web.de>
2024-12-05 22:53:08 +0100

    day5: ex2

--- advent-of-code/adventofcode/src/day5/Day5.java
+++ advent-of-code/adventofcode/src/day5/Day5.java
@@ -14,7 +14,7 @@ public final class Day5 {
     public static void main(String[] args){
         try {
             // Daten aus Datei einlesen
-        String fileName = "C:/Users/tombo/private-Projekte/advent-of-code/advent-of-code/adventofcode/src/day5/input.txt";
+        String fileName = "D:/,private Projekte/Code/advent-of-code/advent-of-code/advent-of-code/adventofcode/src/day5/input.txt";
         Path path = Paths.get(fileName);
         List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
         String[] input = new String[allLines.size()];
@@ -27,7 +27,7 @@ public final class Day5 {
             numbersAfter[Integer.parseInt(num[0])][Integer.parseInt(num[1])] = true;
         }
         //System.out.println(input.length);
-        ex1(input);
+        ex2(input);
         System.out.println("Ergebnis: " + result);
         } catch (Exception e) {
            System.out.println(e.toString());
@@ -54,6 +54,39 @@ public final class Day5 {
                     }
                 }
                 if(l == value.length -1){
+                    System.out.println(Arrays.toString(value));
+                    System.out.println(value[(value.length - 1) / 2]);
+                    result += value[(value.length - 1) / 2];
+                }
+            } 
+        }
+    }
+
+    public static void ex2(String[] input){
+        
+        for (int i = division+1; i < input.length; i++) {
+            boolean firstRun = true;
+            String[] val = input[i].split(",");
+            int[] value = new int[val.length];
+            for (int h = 0; h < val.length; h++) {
+                value[h] = Integer.parseInt(val[h]);
+            }
+            //System.out.println("Zeile " + i + ": " + Arrays.toString(val));
+            nums:
+            for (int l = 0; l < value.length; l++) {
+                int x = value[l];
+                for (int p = 0; p < l; p++) {
+                    int y = value[p];
+                    if(numbersAfter[x][y]){
+                        int help = value[p];
+                        value[p] = value[l];
+                        value[l] = help;
+                        firstRun = false;
+                        l = 0;
+                        continue nums;
+                    }
+                }
+                if(l == value.length -1 && !firstRun){
                     System.out.println(Arrays.toString(value));
                     System.out.println(value[(value.length - 1) / 2]);
                     result += value[(value.length - 1) / 2];
