4b3ab728f28ea52e93d98bc20811dc600a26c22f
Atom <atomcraft.4u@web.de>
2024-12-08 01:07:55 +0100

    day7: ex1, ex2 doesn't work

--- advent-of-code/adventofcode/src/day6/Day6.java
+++ advent-of-code/adventofcode/src/day6/Day6.java
@@ -10,27 +10,58 @@ public final class Day6 {
     public static int result = 0;
     public static int[] guardPosition = new int[3];
     static  boolean end = false;
+    static boolean isRecursive = false;
+    static boolean  count = false;
+
     public static void main(String[] args) throws Exception {
         System.out.println("Nikolaus!");
-
         // Daten aus Datei einlesen
-        String fileName = "advent-of-code/adventofcode/src/day6/input.txt";
+        String fileName = "advent-of-code/adventofcode/src/day6/test.txt";
         Path path = Paths.get(fileName);
         List<String> allLines = Files.readAllLines(path, StandardCharsets.UTF_8);
         char[][] input = new char[allLines.size()][];
         for (int i = 0; i < allLines.size(); i++) {
             input[i] = allLines.get(i).toCharArray();
         }
-        ex1(input);
-        System.out.println("Ergebnis: " + result);System.out.println(input[0][4]);
+        ex2(input);
+        System.out.println("Ergebnis: " + result);
+        char[] k = new char[10];
+
     }
     
     public static void ex1(char[][] input) {
         guardPosition = getGuard(input);
-        while (!end) { 
+        while (!end && !isRecursive) { 
            input = walk(input, guardPosition);
         }
-        result = countCharValue(input, 'X');
+        result = countCharValue(input, '0');
+        result += countCharValue(input, '1');
+        result += countCharValue(input, '2');
+        result += countCharValue(input, '3');
+    }
+
+    public static void ex2(char[][] input) {
+        int[] startGuardPosition = getGuard(input);
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[0].length; j++) {
+                guardPosition = startGuardPosition.clone();
+                if(input[i][j] != '#' && (i != guardPosition[0] || j != guardPosition[1])){
+                    end = false;
+                    isRecursive = false;
+                    char[][] newInput = new char[input.length][];
+                    for (int k = 0; k < input.length; k++) {
+                        newInput[k] = input[k].clone();
+                    }
+                    newInput[i][j] = '#';
+                    while (!end && !isRecursive) {
+                        walk(newInput, guardPosition);
+                    }
+                    if(isRecursive){
+                        result++;
+                    }
+                }
+            }
+        }
     }
 
     public static int[] getGuard(char[][] input){ // output[0] = row, out[1] = column, out[2] = direction
@@ -63,13 +94,17 @@ public final class Day6 {
         int row = guardPosition[0];
         int column = guardPosition[1];
         int direction = guardPosition[2];
-        input[row][column] = 'X'; 
+        if(input[row][column] == Character.forDigit(direction, 10) ){isRecursive = true; return input;}
+        input[row][column] = Character.forDigit(direction, 10); 
         
         switch (direction) {
-            case 0: if (row != 0) { if(!changeGuardDirection(input, row-1, column)) {                     guardPosition[0] = row-1;}} else {end = true;} break;
-            case 1: if (column != input[0].length-1) {if(!changeGuardDirection(input, row, column+1)) {   guardPosition[1] = column+1;}} else {end = true;} break;
-            case 2: if (row != input.length-1) { if(!changeGuardDirection(input, row+1, column)) {        guardPosition[0] = row+1;}} else {end = true;} break;
-            case 3: if (column != 0) {  if(!changeGuardDirection(input, row, column-1)) {                 guardPosition[1] = column-1;}} else {end = true;} break;
+            case 0: if (row != 0) { 
+                if(!changeGuardDirection(input, row-1, column)) { 
+                    guardPosition[0] = row-1;} 
+                    else{input[row][column] = '4';}} else {end = true;} break;
+            case 1: if (column != input[0].length-1) {if(!changeGuardDirection(input, row, column+1)) {   guardPosition[1] = column+1;}else if(count == true){isRecursive = true;}else{count = true;}} else {end = true;} break;
+            case 2: if (row != input.length-1) { if(!changeGuardDirection(input, row+1, column)) {        guardPosition[0] = row+1;}else if(count == true){isRecursive = true;}else{count = true;}} else {end = true;} break;
+            case 3: if (column != 0) {  if(!changeGuardDirection(input, row, column-1)) {                 guardPosition[1] = column-1;}else if(count == true){isRecursive = true;}else{count = true;}} else {end = true;} break;
             default:
         }
         return input;
@@ -89,10 +124,9 @@ public final class Day6 {
 
     public static boolean changeGuardDirection(char[][] input, int x, int y){
         if(input[x][y] == '#'){
-            guardPosition[2]++;
-            if(guardPosition[2] > 3) {
-                guardPosition[2] = 0;
-            } return true;
+            guardPosition[2]++; 
+            guardPosition[2] %= 4;
+            return true;
         }
         return false;
     }
---
+++ advent-of-code/adventofcode/src/day7/Day7.java
@@ -0,0 +1,109 @@
+package day7;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day7 {
+
+    static long result = 0;
+    public static void main(String[] args) throws Exception {
+        
+        //Daten Einlesen aus txt
+        System.out.println("\nDec. 2nd!");
+        String fileName = "advent-of-code/adventofcode/src/day7/input.txt";
+        Path path = Paths.get(fileName);
+        List<String> equatation = Files.readAllLines(path, StandardCharsets.UTF_8);
+        ex2(equatation);
+        System.out.println(result);
+    }
+
+    public static void ex1(List<String> rows){
+        for (String row : rows) {
+            String[] splitted = row.split(" ");
+            long solution = Long.parseLong(splitted[0].replace(":", ""));
+            long[] nums = new long[splitted.length-1];
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = Integer.parseInt(splitted[i+1]);
+            }
+            final int n = nums.length;
+            for (int i = 0; i < Math.pow(2, n); i++) {
+                String bin = Integer.toBinaryString(i);
+                while (bin.length() < n){
+                    bin = "0" + bin;}
+                char[] chars = bin.toCharArray();
+                boolean[] boolArray = new boolean[n];
+                for (int j = 0; j < chars.length; j++) {
+                    boolArray[j] = (chars[j] == '0') ? true : false;
+                }
+                if(calculate(nums, boolArray) == solution){
+                    result += solution;
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void ex2(List<String> rows){
+        for (String row : rows) {
+            String[] splitted = row.split(" ");
+            long solution = Long.parseLong(splitted[0].replace(":", ""));
+            long[] nums = new long[splitted.length-1];
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = Integer.parseInt(splitted[i+1]);
+            }
+
+            final int n = nums.length;
+            for (int i = 0; i < Math.pow(3, n); i++) {
+                String bin = Integer.toString(i, 3);
+                while (bin.length() < n){
+                    bin = "0" + bin;}
+                char[] chars = bin.toCharArray();
+                int[] boolArray = new int[n];
+                for (int j = 0; j < chars.length; j++) {
+                    String hs = Character.toString(chars[j]);
+                    boolArray[j] = Integer.parseInt(hs);
+                }
+
+                if(calculate(nums, boolArray) == solution){
+                    result += solution;
+                    break;
+                } 
+            }
+        }
+    }
+    public static long calculateNumbers(long n1, long n2, boolean isMultiply){
+        if(isMultiply){
+            return n1 * n2;
+        } else{
+            return n1 + n2;
+        }
+    }
+
+    public static long calculateNumbers(long n1, long n2, int operator) {
+        switch(operator){
+            case 0: return n1 + n2;
+            case 1: return n1 * n2;
+            default:
+            case 2: long help = n2 * 10; while(help>10){ n1 *= 10; help /= 10;} return n2 + n1;
+        }
+        
+    }
+
+    public static long calculate(long[] nums, boolean[] operator){
+        long out = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            out = calculateNumbers(out, nums[i], operator[i-1]);
+        }
+        return out;
+    }
+    public static long calculate(long[] nums, int[] operator){
+        long out = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            out = calculateNumbers(out, nums[i], operator[i-1]);
+        }
+        return out;
+    }
+}
